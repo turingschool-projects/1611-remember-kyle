e15c0c88e0fb176846f6a2232077edafdc81c1df
Kyle Zucker <zkyle12@gmail.com>
2017-04-05 15:24:46 -0600

    Complete reminder edit.

--- app/controllers/reminders/edit.js
+++ app/controllers/reminders/edit.js
@@ -3,13 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     updateReminder() {
-      const title = this.get('title');
-      const date = this.get('date');
-      const notes = this.get('notes');
-      this.get('store').findRecord('reminder', this.id)
-      .then((foundReminder) => {
-        foundReminder.set('title', title)
-      })
+      this.transitionToRoute('reminders')
     }
   }
 });
+
+
--- app/models/reminder.js
+++ app/models/reminder.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   notes: DS.attr('string'),
-  date: DS.attr('date')
+  date: DS.attr('string')
 });
